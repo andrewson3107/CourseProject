57bc427a951b0ba55f3d82787dddd266493bb5ce
AndrewSon3107 <andrewson0803@gmail.com>
2021-12-09 16:30:38 -0600

    Updated readme.md

--- frontend_flask/app.py
+++ frontend_flask/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import src.scraper as scraper
+import src.formatter as formatter
+import src.ranker as ranker
 
 app = Flask(__name__)
 
@@ -9,5 +12,14 @@ def index():
 
 @app.route("/rank/<query>", methods=['GET'])
 def rank(query):
-    query
-    return {}
+    ranker.main()
+    
+    output = format_output()
+    return jsonify(output)
+
+def format_output():
+    output = {
+        'Job Name': 'Application Link',
+    }
+
+    return output
